4c53fe625ca1b1dc650b8c11f5a61729ff3431ee
n-coding <ry.nishizono@gmail.com>
2018-08-25 17:48:48 +0900

    jdkコンパイラーバージョンがデフォルトで1.5になってしまう問題を解消。テストクラスを作成。

---
+++ src/test/java/jp/co/training/BookTest.java
@@ -0,0 +1,14 @@
+package jp.co.training;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
